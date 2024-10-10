01faa5c6dca989e753a1f9de3a289975efea22b1
Uthiralakshmi Sivaraman <111710760+UthiraS@users.noreply.github.com>
2024-10-10 13:40:16 -0400

    Delete Matlab directory

--- Matlab/encoderless_VS/binarizeCallback.m
+++
@@ -1,6 +0,0 @@
-function binarizeCallback(~,msg)
-    global binary_img;
-    binary_img = msg; %% TO DO: Add threshold filter
-            
-    msg
-end
--- Matlab/soft_manipulation/skeleton/compute_coeffs.m
+++
@@ -1,9 +0,0 @@
-%% Use this function def when not plotting curves
-%function[curve, coeffs] = compute_coeffs(joint_pos)
-
-%% Use this function def when Plotting curves
-function[curve, coeffs, skel] = compute_coeffs(joint_pos)
-[ee_cart_pos, j2_cart_pos] = compute_cart_pos(joint_pos);
-skel = skeleton(j2_cart_pos, ee_cart_pos);
-[curve, coeffs] = fit_curve(skel);
-end
